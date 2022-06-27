a8cc97e4f856e44553fd67dea4692d00842a82d1
s20226 <101040293+s20226@users.noreply.github.com>
2022-06-28 00:38:30 +0200

    new

---
+++ Controllers/NazwaController.cs
@@ -0,0 +1,24 @@
+﻿using Kolokwium_S20226.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium_S20226.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NazwaController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+        public NazwaController (IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+
+
+    }
+}
