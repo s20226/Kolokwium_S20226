952b5f9e8591792f9f8872a3937e25ea86b1c031
s20226 <101040293+s20226@users.noreply.github.com>
2022-06-27 23:52:50 +0200

    Dodaj pliki projektów.

---
+++ Models/MainDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium_S20226.Models
+{
+    public class MainDbContext : DbContext
+    {
+        protected MainDbContext()
+        {
+
+        }
+
+        public MainDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //DbSety
+        //public DbSet<Musician> Musicians { get; set; }
+        public DbSet<NazwaTabeli> NazwaTabeli { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<NazwaTabeli>(e =>
+            //{
+
+            //});
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<NazwaTabeli>(e =>
+            {
+                e.HasKey(e => e.Id);
+                e.Property(e => e.Nazwa ).IsRequired().HasMaxLength();
+
+                e.HasData(
+                    new NazwaTabeli {
+                    
+                    },
+                    new NazwaTabeli { 
+
+                    }
+                    );
+            });
+
+        }
+    }
+}
